00571a9f764c54e6d9fa63df4616c8ace29a8ce2
Ryong <kjr2436@gmail.com>
2021-04-06 14:03:40 +0900

    210406_css

---
+++ Scripts/member_table.sql
@@ -0,0 +1,15 @@
+select user(), database();
+
+drop table if exists member;
+
+create table member(
+	id varchar(15) not null,
+	password char(41),
+	name varchar(15),
+	age int(11),
+	gender varchar(5),
+	email varchar(30),
+	primary key (id)
+);
+
+desc member;
